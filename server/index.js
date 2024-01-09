1d7ea1caefa651a4d2657b86ec403305afc86397
Colin Cooley <colincooley1989@gmail.com>
2024-01-08 18:48:15 -0900

    fixed port issues for railway deployment

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = 3000;
+const port = process.env.PORT || 3000;
 const router = require('./router');
 
 app.use(cors());
@@ -10,6 +10,6 @@ app.use(express.json());
 app.use(express.static(__dirname + '/../client/build'));
 app.use('/api', router);
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(port, "0.0.0.0"() => {
+  console.log(`Server running on http://0.0.0.0:${port}`);
 });
