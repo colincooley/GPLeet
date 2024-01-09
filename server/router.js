1c9642fc3de7fa95b1d59526f70247783ef22fe0
Colin Cooley <colincooley1989@gmail.com>
2024-01-09 09:42:18 -0900

    added verification for loaderio

--- server/router.js
+++ server/router.js
@@ -4,4 +4,8 @@ const chatController = require('./controllers/chatController');
 
 router.post('/chat/message', chatController.sendMessage);
 
+router.get('/loaderio-a10337aaa2696cf102ad6a27ad6091d4', (req, res) => {
+  res.send('loaderio-a10337aaa2696cf102ad6a27ad6091d4');
+});
+
 module.exports = router;
