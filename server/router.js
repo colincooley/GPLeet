9c334ce9653686aee98394d808e207545515ae2b
Colin Cooley <colincooley1989@gmail.com>
2024-01-09 10:15:46 -0900

    reverted loader route handler

--- server/router.js
+++ server/router.js
@@ -5,7 +5,7 @@ const chatController = require('./controllers/chatController');
 router.post('/chat/message', chatController.sendMessage);
 
 router.get('/loaderio-a10337aaa2696cf102ad6a27ad6091d4', (req, res) => {
-  res.sendFile(__dirname + '/../client/build/loaderio-a10337aaa2696cf102ad6a27ad6091d4.txt');
+  res.send('loaderio-a10337aaa2696cf102ad6a27ad6091d4');
 });
 
 module.exports = router;
